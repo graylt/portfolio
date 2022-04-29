83a7661ae8ff5ec7e7f0b6ed69a428899f783b28
tricia gray <tgray@tricias-MacBook-Pro.local>
2022-04-29 17:45:47 -0400

    updated

---
+++ app.js
@@ -0,0 +1,14 @@
+// -----Info: Resume----- myResume, myStuff
+
+// $('myResume').click('myStuff'); 
+
+function myResume() {
+    let x =document.getElementById('myStuff');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    }else {
+        x.style.display = 'none';
+}
+}
+
+
