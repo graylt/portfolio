7cd8d660f7ba05107ffb99885fc0b2db18b8ea43
tricia gray <tgray@tricias-MacBook-Pro.local>
2022-05-01 14:48:05 -0400

    updated

--- app.js
+++ app.js
@@ -237,8 +237,24 @@ function carousel() {
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}    
   x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 1000); // Change image every 2 seconds
+  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 })
 
+let slideIndex = 1;
+showDivs(slideIndex);
 
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  let i;
+  const x = document.getElementsByClassName("p-1");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
